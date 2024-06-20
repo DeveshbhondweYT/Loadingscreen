86b73ec7dcba9df79b5730d123b8d5635a73adb2
DeveshbhondweYT <101574057+DeveshbhondweYT@users.noreply.github.com>
2024-06-20 13:43:43 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Kakarot'
+description 'CL-Loadingscreen'
+version '1.2.2'
+
+author 'deveshbhondwe' 
+description 'CL-Loadingscreen'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
