9be4a301bb48ad4d3ff097bceb8fb74fe8e651d7
DeveshbhondweYT <101574057+DeveshbhondweYT@users.noreply.github.com>
2024-06-20 13:53:33 +0530

    Add files via upload

---
+++ assets/scripts/config.js
@@ -0,0 +1,42 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "34.93.83.82";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/S7bZQrDmDN"},
+    {name: "instagram", label: "Instagram", description: "An Instagram page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "https://www.instagram.com/clrpindia"},
+    {name: "YouTube", label: "YouTube", description: "Please Subscribe To These Channel", icon: "assets/media/icons/youtube.png", link: "https://www.youtube.com/deveshbhondwe"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Bilal Sabkhan", description: "Owner", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1042418998479945748/avatar-1.jpg?ex=6674cc18&is=66737a98&hm=2980a7db3b9c571de79713b61354661f95653dc2cfa0baba307509d05282ea8e&=&format=webp&width=300&height=300"},
+
+    {name: "Devesh Bhondwe", description: "Co-Owner & Developer", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1026033999228960838/avatar-2.jpg?ex=66752ca0&is=6673db20&hm=5938ef8e8baa41a92b9b539ae03932b581d55de050333940095c4ab5c0182d12&=&format=webp&width=300&height=300"},
+
+    {name: "Saurav Madkar", description: "Modarator", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1042418999192989716/52100214_1.jpg?ex=6674cc18&is=66737a98&hm=cb4917b86f94e1ae5ef3264e1bca51dccdec1296bfbd9039dc8bcf965e28a594&=&format=webp&width=300&height=300"},
+
+    {name: "Pooja Khatri", description: "Streamer", color: "#ff0000", image: "https://e1.pxfuel.com/desktop-wallpaper/20/867/desktop-wallpaper-100-beautiful-indian-girl-pic-now.jpg"},
+
+    {name: "Saurabh Gondane", description: "Assistant Developer & Streamer", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1215654381874053210/image_4_1.png?ex=6674d91e&is=6673879e&hm=ebe82ef1a8717d136827650c426eb7000f4136d4f6190d6418a00807f2445fbe&=&format=webp&quality=lossless&width=300&height=300"},
+
+    {name: "Aduu Baal", description: "Streamer", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1026034036449222656/avatar-8.jpg?ex=66752ca9&is=6673db29&hm=a8711de7ae10f0ca314f745f7aab916e8d1ae56bef6f9776f3870dd80c56467b&=&format=webp&width=300&height=300"},
+
+    {name: "Wolfy", description: "Investor & Streamer", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1042418998643527710/avatar-4.png?ex=6674cc18&is=66737a98&hm=1490f56f556c6fd3782adc45f486bfda3e6acc11bf90ab265cb1eed5bcf10cce&=&format=webp&quality=lossless&width=300&height=300"},
+
+    {name: "Mr Fang", description: "Investor & Streamer", color: "#ff0000", image: "https://media.discordapp.net/attachments/1026033828772446208/1026034051171233792/avatar-13.jpg?ex=66752cac&is=6673db2c&hm=0fa8defc521bd7c80bb38fef83451d405a21bcd4a46176ad2fdfcad7b5854300&=&format=webp&width=300&height=300"},
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Support Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
